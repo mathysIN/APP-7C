37a8940d609e57776281e7cad4f8bc2657dd5a00
Mathys <mathys.gouzenes@eleve.isep.fr>
2023-10-10 18:30:53 +0200

    Added mission 1.7

---
+++ electronique/cahier_manipulation/src/mission_1_7_a.ino/mission_1_7_a.ino.ino
@@ -0,0 +1,18 @@
+#define POTENTIOMETRE PE_3
+#define TENSION_MAX 3.3
+#define VALEUR_NUMERIQUE_MAX 4096
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POTENTIOMETRE, INPUT);
+}
+
+void loop() {
+  int valeurNumerique = analogRead(POTENTIOMETRE);
+  float tension = (float) valeurNumerique * (3.3/VALEUR_NUMERIQUE_MAX) - 1;
+  Serial.print("Tension = ");
+  Serial.print(tension);
+  Serial.print("V");
+  Serial.println("");
+  delay(500);
+}
