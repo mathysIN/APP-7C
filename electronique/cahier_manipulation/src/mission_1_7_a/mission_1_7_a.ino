4940724fa92c9406baabafca5e4dca3081989b40
mathys <mr.red111111@gmail.com>
2023-10-13 15:29:18 +0200

    Fixed mission_1_7_a

--- electronique/cahier_manipulation/src/mission_1_7_a/mission_1_7_a.ino
+++ electronique/cahier_manipulation/src/mission_1_7_a/mission_1_7_a.ino
@@ -4,12 +4,15 @@
 
 void setup() {
   Serial.begin(9600);
-  pinMode(POTENTIOMETRE, INPUT);
+  pinMode(POTENTIOMETRE_PIN, INPUT);
 }
 
 void loop() {
-  int valeurNumerique = analogRead(POTENTIOMETRE);
-  float tension = (float) valeurNumerique * (3.3/VALEUR_NUMERIQUE_MAX) - 1;
+  int valeurNumerique = analogRead(POTENTIOMETRE_PIN);
+  float tension = (float) valeurNumerique * (3.3/VALEUR_NUMERIQUE_MAX);
+  Serial.print("Valeur numérique = ");
+  Serial.print(valeurNumerique);
+  Serial.print(" - ");
   Serial.print("Tension = ");
   Serial.print(tension);
   Serial.print("V");
