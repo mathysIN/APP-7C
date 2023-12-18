3713de7e89b214bce2bb9796b7f57b38716cac25
mathysIN <mathys.gouzenes@gmail.com>
2023-12-18 11:53:26 +0100

    Improved problem 4 code

--- signal/matlab_src/probleme_4/local_libs/calculate_PDBm/test_calculate_PDBm.m
+++ signal/matlab_src/probleme_4/local_libs/calculate_PDBm/test_calculate_PDBm.m
@@ -1,14 +1,28 @@
-addpath('../../../libs');
-init();
+%------------------------------------------
+% Nom du fichier : test_calculate_PDBm.m
+% Groupe :          Groupe 7C
+% Description :     Ce script de test évalue la fonction calculate_PDBm avec des cas de test spécifiques.
+%
+% Entrées :
+%   Aucune (les paramètres sont définis directement dans le code)
+%
+% Sorties :
+%   Aucune (les résultats des tests sont affichés dans la console)
+%
+% Modifiées :       Aucune
+%
+% Locales :
+%   sensibility     Réel        Sensibilité pour le test
+%   gain            Réel        Gain pour le test
+%   seuil_dBSPL     Réel        Seuil en dB SPL pour le test
+%------------------------------------------
 
-S = -67;
-G = 16;
+sensibility = -67;
+gain = 16;
 seuil_dBSPL = 110;
 
-test_all_code([]);
-
 test_all_code([ ...
-    test_code("Calcul du PDBm #1", calculate_PDBm(0, 0, 0), -64), ...
+    test_code("Calcul du PDBm #1", calculate_PDBm(0, 0, 123), -64), ...
     ... For some reasons, we cannot do this test without rounding the number
-    test_code("Calcul du PDBm #2", round(calculate_PDBm(S,G,seuil_dBSPL)), -5) ...
+    test_code("Calcul du PDBm #2", round(calculate_PDBm(sensibility, gain, seuil_dBSPL)), -5) ...
 ]);
--- signal/matlab_src/probleme_4/local_libs/show_data/show_data.m
+++ signal/matlab_src/probleme_4/local_libs/show_data/show_data.m
@@ -1,8 +1,33 @@
+%------------------------------------------
+% Nom du fichier : show_data.m
+% Groupe :          Groupe 7C
+% Description :     Cette fonction analyse un signal audio et affiche des informations sur sa puissance,
+%                   en indiquant si le signal est considéré comme pénible. Elle affiche également le signal
+%                   audio original et le signal filtré pour les fréquences supérieures à 2 kHz.
+%
+% Entrées :
+%   y           Vecteur     Signal audio original
+%   y_filtered  Vecteur     Signal audio filtré (fréquences > 2 kHz)
+%   Fs          Réel        Fréquence d'échantillonnage du signal en Hertz
+%   PDBm        Réel        Puissance en dBm à partir de la fonction calculate_PDBm
+%
+% Sorties :
+%   Aucune (les résultats sont affichés dans la console et les signaux sont tracés)
+%
+% Modifiées :       Aucune
+%
+% Locales :
+%   puissance_totale       Réel        Puissance totale du signal audio original
+%   puissance_sup_2kHz     Réel        Puissance du signal audio filtré pour les fréquences > 2 kHz
+%   pourcentage_sup_2kHz   Réel        Pourcentage de la puissance supérieure à 2 kHz par rapport à la puissance totale
+%   t                      Vecteur     Vecteur de temps pour les signaux audio
+%------------------------------------------
+
 function show_data(y,y_filtered, Fs, PDBm)
     puissance_totale = sum(y.^2);
     puissance_sup_2kHz = sum(y_filtered.^2);
     pourcentage_sup_2kHz = (puissance_sup_2kHz / puissance_totale) * 100;
-    
+        
     if puissance_totale > PDBm && pourcentage_sup_2kHz > 20
         disp("Le signal est pénible");
     else
@@ -25,4 +50,3 @@ function show_data(y,y_filtered, Fs, PDBm)
     ylabel("Amplitude");
     title("Signal audio filtré (fréquences > 2 kHz)")
 end
-
