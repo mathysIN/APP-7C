a53445e6eb920eac0e4dd5e64641d6cfcfa4b6c3
ninalemoigne <lemoigne.nina@gmail.com>
2024-02-23 17:20:04 +0100

    feat/ new style for login

--- informatique/api/public/login.php
+++ informatique/api/public/login.php
@@ -1,16 +1,21 @@
-<div class="h-[100vh] flex flex-col items-center justify-center">
-    <div class="max-w-md mx-auto p-6">
+<div class="h-[100vh] flex flex-col items-center justify-top pt-20">
+    <div class="max-w-md mx-auto p-2">
+        <img class="w-80" src="/resources/logo-event-it.webp">
         <form>
             <div class="mb-4">
-                <label for="email" class="block text-eventit-800">Email</label>
-                <input type="email" id="email" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="email" class="block text-eventit-500">Email</label>
+                <input type="email" id="email" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
-            <div class="mb-4">
-                <label for="password" class="block text-eventit-800">Password</label>
-                <input type="password" id="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+            <div class="mb-4 pb-5">
+                <label for="password" class="block text-eventit-500">Password</label>
+                <input type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
-            <button type="submit" class="w-full bg-eventit-500 text-white py-2 px-4 rounded-xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500">Login</button>
+            <div class="text-center">
+                <button type="submit" class="w-2/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500">Login</button>
+            </div>
         </form>
-        <a href="create_account" class="mt-4 text-center text-eventit-800">Create an account</p></a>
+        <div class="text-center underline">
+            <a href="create_account" class="mt-4 text-center text-eventit-500">Create an account</p></a>
+        </div>
     </div>
 </div>
