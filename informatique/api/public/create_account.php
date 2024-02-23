5cfc3876124cb6c33709f90ab2b93ebe42f5f185
ninalemoigne <lemoigne.nina@gmail.com>
2024-02-23 17:19:48 +0100

    feat/ new style for create account

--- informatique/api/public/create_account.php
+++ informatique/api/public/create_account.php
@@ -1,28 +1,30 @@
-<div class="h-[100vh] flex flex-col items-center justify-center">
+<div class="h-[100vh] flex flex-col items-center justify-top">
     <div class="max-w-md mx-auto p-6">
+    <img class="w-80" src="/resources/logo-event-it.webp">
         <form>
             <div class="mb-4">
-                <label for="email" class="block text-eventit-800">First Name</label>
-                <input type="text" id="email" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="email" class="block text-eventit-500">First Name</label>
+                <input type="text" id="email" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-eventit-800">Last Name</label>
-                <input type="text" id="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500">Last Name</label>
+                <input type="text" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-eventit-800">Email</label>
-                <input type="email" id="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500">Email</label>
+                <input type="email" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-eventit-800">Password</label>
-                <input type="password" id="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500">Password</label>
+                <input type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-eventit-800">Password</label>
-                <input type="password" id="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500">Password</label>
+                <input type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
-            <button type="submit" class="w-full bg-eventit-500 text-white py-2 px-4 rounded-xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500">Create account</button>
+            <div class="text-center">
+                <button type="submit" class="w-3/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500">Create account</button>
+            </div>
         </form>
-        <a href="login" class="mt-4 text-center text-eventit-800">Login</p></a>
     </div>
 </div>
