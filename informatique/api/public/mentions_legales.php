b4cdf8adc3f947641e4ae8760b49fcdc933158d0
teobrdd <teobernard2300@icloud.com>
2024-04-04 01:42:13 +0200

    jsuis un enculé ptn

--- informatique/api/public/mentions_legales.php
+++ informatique/api/public/mentions_legales.php
@@ -8,4 +8,20 @@
         <p class="text-xl text-left" data-lang="N° CNIL : 1234567|CNIL Number: 1234567">N° CNIL : 1234567</p>
         <p class="text-xl text-left" data-lang="SIRET : 123 456 789 01234|SIRET: 123 456 789 01234">SIRET : 123 456 789 01234</p>
         <p class="text-xl text-left" data-lang="Téléphone : (+33) 01 23 45 67 89|Phone: (+33) 01 23 45 67 89">Téléphone : (+33) 01 23 45 67 89</p>
-        <p class="text-
+        <p class="text-xl text-left" data-lang="N° TVA : FR 123456789|VAT Number: FR 123456789">N° TVA : FR 123456789</p>
+    </div>
+    <div class="px-44 pt-10 p-6 self-left">
+        <h2 class="text-eventit-500" data-lang="Hébergeurs|Hosts">Hébergeurs</h2>
+        <p class="text-xl text-left" data-lang="Heroku, Salesforce Company|Heroku, Salesforce Company">Heroku, Salesforce Company</p>
+        <p class="text-xl text-left" data-lang="Adresse : 50 Fremont St., San Francisco, CA 94105, États-Unis|Address: 50 Fremont St., San Francisco, CA 94105, USA">Adresse : 50 Fremont St., San Francisco, CA 94105, États-Unis</p>
+        <p class="text-xl text-left" data-lang="Téléphone : +1 (415) 738-2070|Phone: +1 (415) 738-2070">Téléphone : +1 (415) 738-2070</p>
+        <p class="text-xl text-left" data-lang="Site web : https://www.heroku.com|Website: https://www.heroku.com">Site web : https://www.heroku.com</p>
+    </div>
+    <div class="px-44 pt-10 pb-20 p-6 self-left">
+        <h2 class="text-eventit-500" data-lang="Cloud|Cloud">Cloud</h2>
+        <p class="text-xl text-left" data-lang="Amazon Web Services (AWS)|Amazon Web Services (AWS)">Amazon Web Services (AWS)</p>
+        <p class="text-xl text-left" data-lang="Adresse : 410 Terry Ave North, Seattle, WA 98109, États-Unis|Address: 410 Terry Ave North, Seattle, WA 98109, USA">Adresse : 410 Terry Ave North, Seattle, WA 98109, États-Unis</p>
+        <p class="text-xl text-left" data-lang="Téléphone : +1 (206) 266-1000|Phone: +1 (206) 266-1000">Téléphone : +1 (206) 266-1000</p>
+        <p class="text-xl text-left" data-lang="Site web : https://aws.event-it.com|Website: https://aws.event-it.com">Site web : https://aws.event-it.com</p>
+    </div>
+</div>
