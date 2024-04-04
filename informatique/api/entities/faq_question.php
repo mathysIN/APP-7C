c62f5a889d06b82e78e595a0e8a274f0ffedc886
mathys <mr.red111111@gmail.com>
2024-04-04 16:31:36 +0200

    Added ids to FAQ questions

--- informatique/api/entities/faq_question.php
+++ informatique/api/entities/faq_question.php
@@ -5,6 +5,7 @@ class FAQQuestion
     public $section;
     public $question;
     public $answer;
+    public $id;
 }
 
 class FAQAPI
@@ -16,21 +17,28 @@ class FAQAPI
         $this->pdo = $pdo;
     }
 
-    public function getFAQQuestion()
+    private function toFAQQuestion($row)
     {
-        $stmt = $this->pdo->query("SELECT * FROM FAQ_Question LIMIT 1");
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if (!$row) {
-            return null; // No FAQ question found
-        }
-
         $faqQuestion = new FAQQuestion();
         $faqQuestion->section = $row['section'];
         $faqQuestion->question = $row['question'];
         $faqQuestion->answer = $row['answer'];
+        $faqQuestion->id = $row['id'];
 
         return $faqQuestion;
+    }
+
+    public function getFAQQuestionById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM FAQ_Question WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$row) {
+            return null;
+        }
+
+        return $this->toFAQQuestion($row);
     }
 
     public function getAllFAQQuestions()
@@ -41,24 +49,25 @@ class FAQAPI
         $faqQuestions = [];
 
         foreach ($rows as $row) {
-            $faqQuestion = new FAQQuestion();
-            $faqQuestion->section = $row['section'];
-            $faqQuestion->question = $row['question'];
-            $faqQuestion->answer = $row['answer'];
-
-            $faqQuestions[] = $faqQuestion;
+            $faqQuestions[] = $this->toFAQQuestion($row);
         }
 
         return $faqQuestions;
+    }
+
+    public function updateFAQQuestion($id, $section, $question, $answer)
+    {
+        $stmt = $this->pdo->prepare("UPDATE FAQ_Question SET section = :section, question = :question, answer = :answer WHERE id = :id");
+        $stmt->execute(['section' => $section, 'question' => $question, 'answer' => $answer, 'id' => $id]);
     }
 }
 
 const QUERY_CREATE_TABLE_FAQ = "CREATE TABLE FAQ_Question (
     section VARCHAR(255),
     question VARCHAR(255),
-    answer VARCHAR(255)
+    answer VARCHAR(255),
+    id INT AUTO_INCREMENT PRIMARY KEY
 );";
 
 const QUERY_DEFAULT_FAQ_VALUE = "INSERT INTO FAQ_Question (section, question, answer) 
-VALUES ('General', 'What is Lorem Ipsum?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.');
-";
+VALUES ('General', 'What is Lorem Ipsum?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.');";
