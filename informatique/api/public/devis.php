9d365589eebe4665f818449de5063017f0075a74
ninalemoigne <lemoigne.nina@gmail.com>
2024-04-05 20:17:58 +0200

    style and home page

--- informatique/api/public/devis.php
+++ informatique/api/public/devis.php
@@ -26,38 +26,75 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="max-w-md mx-auto p-6 text-center">
         <form method="post">
             <div class="mb-4">
-                <label for="organization" class="block text-left pl-1 text-eventit-500" data-lang="Votre organisation|Your organization">Votre organisation</label>
-                <input type="text" id="organization" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="organization" class="block text-left pl-1 text-eventit-500"
+                    data-lang="Votre organisation|Your organization">Votre organisation</label>
+                <input type="text" id="organization"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="fullname" class="block text-left pl-1 text-eventit-500" data-lang="Nom et Prénom|First and last name">Nom et Prénom</label>
-                <input type="text" id="fullname" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="fullname" class="block text-left pl-1 text-eventit-500"
+                    data-lang="Nom et Prénom|First and last name">Nom et Prénom</label>
+                <input type="text" id="fullname"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
                 <label for="email" class="block text-left pl-1 text-eventit-500">Email</label>
-                <input type="email" id="email" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input type="email" id="email"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="phone" class="block text-left pl-1 text-eventit-500" data-lang="Numéro de téléphone|Phone number">Numéro de téléphone</label>
-                <input type="text" id="phone" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="phone" class="block text-left pl-1 text-eventit-500"
+                    data-lang="Numéro de téléphone|Phone number">Numéro de téléphone</label>
+                <input type="text" id="phone"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="rooms" class="block text-left pl-1 text-eventit-500" data-lang="Nombre de salle/endroit|Number of rooms/locations">Nombre de salle/endroit</label>
-                <input class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500" type="number" id="rooms" name="rooms" min="1" max="100">
+                <label for="rooms" class="block text-left pl-1 text-eventit-500"
+                    data-lang="Nombre de salle/endroit|Number of rooms/locations">Nombre de salle/endroit</label>
+                <input class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500" type="number" id="rooms"
+                    name="rooms" min="1" max="100">
             </div>
             <div class="mb-4">
-                <label for="squareMeters" class="block text-left pl-1 text-eventit-500" data-lang="Nombre de m2|Number of m2">Nombre de m2</label>
-                <input class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500" type="number" id="squareMeters" name="squareMeters" min="1" max="100">
+                <label for="squareMeters" class="block text-left pl-1 text-eventit-500"
+                    data-lang="Nombre de m2|Number of m2">Nombre de m2</label>
+                <input class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500" type="number" id="squareMeters"
+                    name="squareMeters" min="1" max="100">
             </div>
             <div class="mb-4">
                 <label for="message" class="block text-left pl-1 text-eventit-500">Message</label>
-                <textarea id="message" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500"></textarea>
+                <textarea id="message"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500"></textarea>
             </div>
             <!-- Ajout d'un bouton de soumission pour le formulaire -->
             <div class="text-center">
-                <button type="submit" class="px-4 py-2 bg-eventit-500 text-white rounded-full focus:outline-none"data-lang="Envoyer|Send">Envoyer</button>
+                <button type="submit" class="px-4 py-2 bg-eventit-500 text-white rounded-full focus:outline-none button"
+                    data-lang="Envoyer|Send">Envoyer</button>
             </div>
         </form>
     </div>
 </div>
+<style>
+    .button {
+        border: none;
+        cursor: pointer;
+        overflow: hidden;
+        position: relative;
+        z-index: 1;
+    }
 
+    .button::before {
+        content: "";
+        z-index: -2;
+        position: absolute;
+        top: 0;
+        left: -100%;
+        width: 100%;
+        height: 100%;
+        background-color: #317577;
+        transition: left 0.3s ease;
+    }
+
+    .button:hover::before {
+        left: 0;
+    }
+</style>
--- informatique/api/public/login.php
+++ informatique/api/public/login.php
@@ -25,18 +25,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form method="post">
             <div class="mb-4">
                 <label for="email" class="block text-eventit-500">Email</label>
-                <input name="email" type="email" id="email" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input name="email" type="email" id="email"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4 pb-5">
-                <label for="password" class="block text-eventit-500"data-lang="Mot de passe|Password">Mot de passe</label>
-                <input name="password" type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500" data-lang="Mot de passe|Password">Mot de
+                    passe</label>
+                <input name="password" type="password" id="password"
+                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="text-center">
-                <button type="submit" class="w-4/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500"data-lang="Se connecter|Login">Se connecter</button>
+                <button type="submit"
+                    class="w-4/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500 button"
+                    data-lang="Se connecter|Login">Se connecter</button>
             </div>
         </form>
         <div class="text-center underline">
-            <a href="create_account" class="mt-2 text-center text-eventit-500"data-lang="Créer un compte|Create account">Créer un compte</a>
+            <a href="create_account" class="mt-2 text-center text-eventit-500"
+                data-lang="Créer un compte|Create account">Créer un compte</a>
         </div>
     </div>
 </div>
+<style>
+    .button {
+        border: none;
+        cursor: pointer;
+        overflow: hidden;
+        position: relative;
+        z-index: 1;
+    }
+
+    .button::before {
+        content: "";
+        z-index: -2;
+        position: absolute;
+        top: 0;
+        left: -100%;
+        width: 100%;
+        height: 100%;
+        background-color: #317577;
+        transition: left 0.3s ease;
+    }
+
+    .button:hover::before {
+        left: 0;
+    }
+</style>
