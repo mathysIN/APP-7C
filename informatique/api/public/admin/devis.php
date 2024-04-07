45c13fe1e00f33c4befdab5f013ed7e80528de62
mathys <mr.red111111@gmail.com>
2024-04-07 18:01:45 +0200

    Fixed more responsive

--- informatique/api/public/admin/devis.php
+++ informatique/api/public/admin/devis.php
@@ -6,22 +6,22 @@ require_once __DIR__ . "/../../utils/helpers.php";
 $estimates = $estimateAPI->getAllEstimates();
 ?>
 
-<div class="flex flex-col">
-    <header class="flex h-14 lg:h-[60px] items-center gap-4 border-b px-6">
-        <a class="lg:hidden" href="#" rel="ugc">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
-                <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z"></path>
-                <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.79 1.1L21 9"></path>
-                <path d="M12 3v6"></path>
-            </svg>
-            <span class="sr-only">Maison</span>
-        </a>
-        <div class="flex-1">
-            <h1 class="font-semibold text-lg" data-lang>Options générales</h1>
-            <p class="text-sm text-gray-500 dark:text-gray-400">
-                Gérer les options du site
-            </p>
-        </div>
+<div class="flex flex-col w-full">
+    <header class=" flex h-14 lg:h-[60px] items-center gap-4 border-b px-6">
+    <a class="lg:hidden" href="#" rel="ugc">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+            <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z"></path>
+            <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.79 1.1L21 9"></path>
+            <path d="M12 3v6"></path>
+        </svg>
+        <span class="sr-only">Maison</span>
+    </a>
+    <div class="flex-1">
+        <h1 class="font-semibold text-lg" data-lang>Options générales</h1>
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+            Gérer les options du site
+        </p>
+    </div>
     </header>
     <main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
         <div class="border shadow-sm rounded-lg">
