67c8c036a8b78307ded94ed8587b1626fdd698dd
mathys <mr.red111111@gmail.com>
2024-04-14 22:53:42 +0200

    Fixed user creation page

--- informatique/api/public/create_account.php
+++ informatique/api/public/create_account.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/../utils/global_types.php";
 require_once __DIR__ . "/../utils/helpers.php";
+require_once __DIR__ . "/../entities/all_entites.php";
 
 if ($_CURRENT_USER) {
     redirect('/');
@@ -8,8 +9,6 @@ if ($_CURRENT_USER) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    require_once __DIR__ . "/../entities/users.php";
 
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -59,40 +58,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form method="post">
             <div class="mb-4">
                 <label for="text" class="block text-eventit-500" data-lang="Nom|Name">Nom</label>
-                <input name="first_name" type="text" id="email"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input name="first_name" type="text" id="email" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-eventit-500" data-lang="Prénom|First name">Prénom</label>
-                <input name="last_name" type="text" id="password"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input name="last_name" type="text" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-eventit-500">Email</label>
-                <input name="email" type="email" id="password"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input name="email" type="email" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
                 <label for="tel" class="block text-eventit-500" data-lang="Téléphone|Phone">Téléphone</label>
-                <input name="phone_number" type="tel" id="password"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input name="phone_number" type="tel" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-eventit-500" data-lang="Mot de passe|Password">Mot de
                     passe</label>
-                <input type="password" id="password"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <input type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="mb-4">
-                <label for="password" class="block text-eventit-500"
-                    data-lang="Confirmation du mot de passe|Password confirmation">Confirmation du mot de passe</label>
-                <input name="password" type="password" id="password"
-                    class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
+                <label for="password" class="block text-eventit-500" data-lang="Confirmation du mot de passe|Password confirmation">Confirmation du mot de passe</label>
+                <input name="password" type="password" id="password" class="w-80 h-9 px-2 py-2 border rounded-3xl border-eventit-500 focus:outline-none focus:ring focus:border-eventit-500">
             </div>
             <div class="text-center">
-                <button type="submit"
-                    class="w-3/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500 button"
-                    data-lang="Créer le compte|Create account">Créer le compte</button>
+                <button type="submit" class="w-3/5 bg-eventit-500 text-white py-2 px-4 rounded-3xl hover:bg-eventit-600 focus:outline-none focus:ring focus:border-eventit-500 button" data-lang="Créer le compte|Create account">Créer le compte</button>
             </div>
         </form>
         <div class="text-center underline">
